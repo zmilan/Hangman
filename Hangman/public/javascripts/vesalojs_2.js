90fe0bfec82b57a2da4fc0acc1fc0c59be4d5fd0
Stevan Vostic <stevanvostic2000@gmail.com>
2017-01-06 20:18:41 +0100

    Added ajax requests so that the app runs asynchronously

--- Hangman/public/javascripts/vesalojs_2.js
+++ Hangman/public/javascripts/vesalojs_2.js
@@ -1,17 +1,34 @@
-var inp = $("#q").val();
-if (inp !== '' && inp !== undefined){
-    postdata(inp);
-}
-function postdata(lele) {
-    $.ajax({
-        type:'POST',
-        url: 'http://localhost:3000/gameover',
-        data: {data:lele},
-        success: function (where) {
-            window.location.replace('gameover/' + where);
-        },
-        /*complete: function() {
-
-        }*/
+var Hangman = (function () {
+    let API = [];
+    const Sbtn = $('#search');
+    const underlinesH = $('#underlines');
+    const usedLeetersH = $('#usedLeeters');
+    const alertH = $('#alert');
+    Sbtn.on('click', function () {
+        API.postInfo();
+        $('#textbox').val('');
     });
-}
+    API.postInfo = function () {
+        let letter = $('#textbox').val();
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:3000/guess',
+            data: {letter: letter},
+            success: function (data) {
+                usedLeetersH.html(data.usedletters.join(','));
+                underlinesH.html(data.underlines.join(' '));
+                alertH.html(data.alert);
+                if (data.count > 0) {
+                    let num = $('#img' + data.count);
+                    num.css('display', 'block');
+                    if (data.count === 6) {
+                        window.location.replace('gameover/lost');
+                    }
+                }
+                if (data.status == 'won') {
+                    window.location.replace('gameover/won');
+                }
+            },
+        });
+    }
+})();
--- Hangman/routes/index.js
+++ Hangman/routes/index.js
@@ -14,46 +14,30 @@ router.post('/user', function (req, res) {
     var propNames = Object.keys(req.body);
     var names = req.app.datafile[propNames];
     var random = names[Math.floor(Math.random() * names.length)].toLowerCase();
-    var underCount = function () {
-        var under = new Array();
-        for (var i = 0; i < random.length; i++) {
-            under[i] = "_";
-        }
-        return under;
-    };
+        var underlines = function () {
+            var array = new Array();
+            for(let i = 0; i<random.length; i++){
+                array[i] = '_';
+            }
+            return array;
+        };
     req.session.name = {
         word: random,
-        underlines: underCount(),
+        underlines: underlines(),
         underNumber: random.length,
         usedletters: [],
-        imagesShown:{
-            img1: 'none',
-            img2: 'none',
-            img3: 'none',
-            img4: 'none',
-            img5: 'none',
-            img6: 'none'},
-        count : 0,
-        alert: "You can do it!",
-        status: ''
+        count: 0,
+        alert: "You can do it!"
     };
     res.redirect('vesalo');
-    console.log(req.session);
 });
 //game is accesed
 router.get('/vesalo', function (req, res) {
-        res.render('user', {
-            underlines: req.session.name.underlines,
-            usedletters: req.session.name.usedletters,
-            img1: req.session.name.imagesShown.img1,
-            img2: req.session.name.imagesShown.img2,
-            img3: req.session.name.imagesShown.img3,
-            img4: req.session.name.imagesShown.img4,
-            img5: req.session.name.imagesShown.img5,
-            img6: req.session.name.imagesShown.img6,
-            alert : req.session.name.alert,
-            status: req.session.name.status
-        });
+    res.render('user', {
+        underlines: req.session.name.underlines.join(' '),
+        usedletters: req.session.name.usedletters,
+        alert: req.session.name.alert
+    });
 });
 router.post('/guess', function (req, res) {
     req.session.name.alert = 'You can do it!';
@@ -67,52 +51,42 @@ router.post('/guess', function (req, res) {
             req.session.name.alert = 'You already used that letter!';
         } else {
             req.session.name.usedletters.push(letter);
-            if (req.session.name.word.indexOf(letter)>= 0){
-                for (var i = 0; i < req.session.name.word.length; i++) {
-                    if(req.session.name.word[i] == letter){
+            if (req.session.name.word.indexOf(letter) >= 0) {
+                for (let i = 0; i < req.session.name.word.length; i++) {
+                    if (req.session.name.word[i] == letter) {
                         req.session.name.underlines[i] = letter;
                         req.session.name.underNumber--;
-                        if (req.session.name.underNumber === 0){
+                        if (req.session.name.underNumber === 0) {
                             req.session.name.status = 'won';
                         }
                     }
                 }
-            }else{
-                console.log('That letter isnt in the word');
+            } else {
+                req.session.name.alert = 'That letter isnt in the word!';
                 req.session.name.count++;
-                if (req.session.name.count === 6){
-                    req.session.name.status = 'lost';
-                }
-                let lele = 'img' + req.session.name.count.toString();
-                req.session.name.imagesShown[lele] = 'block';
             }
         }
     }
-    //render the page
-        res.render('user', {
-            underlines: req.session.name.underlines,
-            usedletters: req.session.name.usedletters,
-            img1: req.session.name.imagesShown.img1,
-            img2: req.session.name.imagesShown.img2,
-            img3: req.session.name.imagesShown.img3,
-            img4: req.session.name.imagesShown.img4,
-            img5: req.session.name.imagesShown.img5,
-            img6: req.session.name.imagesShown.img6,
-            alert: req.session.name.alert,
-            status: req.session.name.status
-        });
+    res.send({
+        underlines: req.session.name.underlines,
+        usedletters: req.session.name.usedletters,
+        alert: req.session.name.alert,
+        count: req.session.name.count,
+        status: req.session.name.status
+    });
+
 });
-router.post('/gameover', function(req,res) {
+router.post('/gameover', function (req, res) {
     res.send(req.body.data);
 });
-router.get('/gameover/:id', function(req, res) {
+router.get('/gameover/:id', function (req, res) {
     if (req.params.id == 'lost') {
         var str = 'Game over, you lose!';
         var wrd = 'The wanted word was ' + '\'' + req.session.name.word + '\'';
-    } else if (req.params.id == 'won'){
+    } else if (req.params.id == 'won') {
         var str = 'You won!';
-        var wrd = 'The wanted word was ' + '\'' + req.session.name.word + '\'' + ', good job' ;
-    }else{
+        var wrd = 'The wanted word was ' + '\'' + req.session.name.word + '\'' + ', good job';
+    } else {
         return;
     }
     res.render('gameover', {
